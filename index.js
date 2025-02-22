7cd41d23e6ef4f7d994243e3cac4670f7476376d
hitesh <hiteshtripathi91@gmail.com>
2025-02-23 00:05:39 +0530

    a message array added and index tempete in / route

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine','ejs')
+
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+
 app.get('/',(req,res)=>{
-    res.send('hello')
+    res.render('index', {messages})
 })
 
 app.get('/new',(req,res)=>{
